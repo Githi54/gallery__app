2118f23edf4d5dd6fc60d7c320e446f3f655c1d9
Nikita Havryliuk <chinter.games1@gmail.com>
2022-12-01 18:44:11 +0200

    fixed code style

--- src/Components/IndividualImage/IndividualImage.tsx
+++ src/Components/IndividualImage/IndividualImage.tsx
@@ -7,7 +7,15 @@ type Props = {
 export const IndividualImage: React.FC<Props> = ({ image }) => {
   return (
       <a href={image.urls.small}>
-        <img src={`${image.urls.small}`} alt={`${image.user.name}`} className='w-100 shadow-1-strong rounded mb-4' />
+        <img 
+          src={`${image.urls.small}`} 
+          alt={`${image.user.name}`} 
+          className='
+            w-100 
+            shadow-1-strong
+            rounded
+            mb-4' 
+        />
       </a>
   );
 };
